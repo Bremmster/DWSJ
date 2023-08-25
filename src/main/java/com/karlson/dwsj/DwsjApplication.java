0a0fbdb757b36bfbd91b73fde010495e5c8f9198
Kristian Karlson <107461637+Bremmster@users.noreply.github.com>
2023-08-25 13:45:24 +0200

    spring proj add

---
+++ src/main/java/com/karlson/dwsj/DwsjApplication.java
@@ -0,0 +1,13 @@
+package com.karlson.dwsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwsjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karlson/dwsj/DwsjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlson.dwsj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwsjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
