ce6d393c225b6f273e139c47799f8ed4d894445a
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-15 16:01:57 +0200

    refactoring of pokemonTypeConverter

--- Pokemon-Consume-SQL/src/main/java/com/karlson/converter/PokemonTypeConverter.java
+++ Pokemon-Consume-SQL/src/main/java/com/karlson/converter/PokemonTypeConverter.java
@@ -1,4 +1,4 @@
-package com.karlson.coverter;
+package com.karlson.converter;
 
 import com.karlson.entity.Pokemon;
 import com.karlson.entity.PokemonType;
@@ -6,10 +6,10 @@ import com.karlson.repository.TypeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
 public class PokemonTypeConverter {
 
@@ -24,17 +24,21 @@ public class PokemonTypeConverter {
 
     public Pokemon typeConverter(Pokemon pokemon) { // ta emot en pokemon
         List<PokemonType> pokemonTypesDb = typeRepository.findAll();  // Hämta lista över typer från databas och förvara statiskt i minnen? eller ta ifrån config?
+        String[] slot = new String[2];
 
-        String slotA = pokemon.getTypes().get(0).getType(); // string
-        String slotB = pokemon.getTypes().get(1).getType(); // string
+        for (int i = 0; i < 2; i++) {
+        slot[i] = pokemon.getTypes().get(i).getType(); // string
+
+        }
+        // String slotA = pokemon.getTypes().get(1).getType(); // string
 
         // jämför värden
-        for (PokemonType type : pokemonTypesDb) {
-            if (type.getType().equalsIgnoreCase(slotA)) {
-                pokemon.setTypeA((int) type.getId());
+        for (PokemonType dbType : pokemonTypesDb) {
+            if (dbType.getType().equalsIgnoreCase(slot[0])) {
+                pokemon.setTypeA((int) dbType.getId());
             }
-            if (type.getType().equalsIgnoreCase(slotB)) {
-                pokemon.setTypeB((int) type.getId());
+            if (dbType.getType().equalsIgnoreCase(slot[1])) {
+                pokemon.setTypeB((int) dbType.getId());
             }
         }
         return pokemon;
