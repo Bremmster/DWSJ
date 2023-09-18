2d082d30a5a7ce55fdeeacbcfccde5b5231510b5
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-18 20:36:28 +0200

    save before reboot

--- Pokemon-Consume-SQL/src/main/java/com/karlson/PokemonConsumerSQL.java
+++ Pokemon-Consume-SQL/src/main/java/com/karlson/PokemonConsumerSQL.java
@@ -1,7 +1,9 @@
 package com.karlson;
 
 import com.karlson.entity.Pokemon;
+import com.karlson.entity.PokemonType;
 import com.karlson.repository.PokemonRepository;
+import com.karlson.repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,28 +12,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Optional;
 
 @SpringBootApplication
-public class PokemonConsumerSQL implements CommandLineRunner {
+public class PokemonConsumerSQL { //implements CommandLineRunner {
 
     PokemonRepository pokemonRepository;
+    TypeRepository typeRepository;
+
+    public PokemonConsumerSQL(PokemonRepository pokemonRepository, TypeRepository typeRepository) {
+        this.pokemonRepository = pokemonRepository;
+        this.typeRepository = typeRepository;
+    }
 
     @Autowired
-    public PokemonConsumerSQL(PokemonRepository pokemonRepository) {
-        this.pokemonRepository = pokemonRepository;
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(PokemonConsumerSQL.class);
     }
-
+/*
     @Override
     public void run(String... args) {
         System.out.println("Welcome");
         Optional<Pokemon> poke = this.pokemonRepository.findById(1L);
-        System.out.println("Found");
         poke.ifPresent(pokemon -> System.out.println(pokemon.getName()));
-        System.out.println("types");
-        poke.ifPresent(pokemon -> System.out.println(pokemon.getTypes()));
+        System.out.println("done");
+        Optional<PokemonType> type = this.typeRepository.findById(1L);
+        for (PokemonType pType: poke.get().getTypes() ) {
+            System.out.println(pType.getType());
+
+        }
+       // poke.ifPresent(pokemon -> System.out.println(pokemon.getTypes()));
     }
+
+ */
 
 
 }
