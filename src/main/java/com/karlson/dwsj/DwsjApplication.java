b8fce776e39c2a38cf11e018a447ed09bcfe3555
Kristian Karlson <107461637+Bremmster@users.noreply.github.com>
2023-09-20 14:40:57 +0200

    first test works!

--- src/main/java/com/karlson/dwsj/DwsjApplication.java
+++ src/main/java/com/karlson/dwsj/DwsjApplication.java
@@ -1,13 +1,19 @@
 package com.karlson.dwsj;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DwsjApplication {
+public class DwsjApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DwsjApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("Hello, world!");
 	}
 
 }
