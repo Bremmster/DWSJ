8f062de94498b93cbeed562c1e92cc2c23b54c7b
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-22 11:22:59 +0200

    deleted unused code

--- src/main/java/com/karlson/dwsj/DwsjApplication.java
+++ src/main/java/com/karlson/dwsj/DwsjApplication.java
@@ -9,30 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Random;
 
 @SpringBootApplication
-public class DwsjApplication implements CommandLineRunner {
+public class DwsjApplication { // implements CommandLineRunner
 
     public static void main(String[] args) {
         SpringApplication.run(DwsjApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args)  {
-        System.out.println("Hello, world!");
-        Random random = new Random();
-
-        // skapa typer
-        Pokemon pokemon = new Pokemon("test", random);
-
-        System.out.println(pokemon);
-        RestClient client = new RestClient();
-        client.sendMessage(pokemon);
-
-
-        // fråga om namn
-
-        // fråga användare om skicka till api
-
-        // skicka till api
-
     }
 }
