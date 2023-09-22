47f88843c4dfc65b224c8aa66e2d2e53cab777aa
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-22 11:42:48 +0200

    both starts

--- src/main/java/com/karlson/dwsj/DwsjApplication.java
+++ src/main/java/com/karlson/dwsj/DwsjApplication.java
@@ -1,12 +1,9 @@
 package com.karlson.dwsj;
 
-import com.karlson.dwsj.model.Pokemon;
-import com.karlson.dwsj.service.RestClient;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Random;
 
 @SpringBootApplication
 public class DwsjApplication { // implements CommandLineRunner
