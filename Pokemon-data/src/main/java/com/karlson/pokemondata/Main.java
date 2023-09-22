6e299843ca150c3f011dddaa6c4d3aa33e70ff99
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-22 15:42:18 +0200

    recreated main in data module, must be there

---
+++ Pokemon-data/src/main/java/com/karlson/pokemondata/Main.java
@@ -0,0 +1,7 @@
+package com.karlson.pokemondata;
+
+public class Main {
+    public static void main(String[] args) {
+        // Package only holds POJOs and ways to create them at with random properties
+    }
+}
