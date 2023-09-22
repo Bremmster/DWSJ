238f92c10325f6b72d2eb425b170d41bfd88b8b9
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-22 15:50:56 +0200

    final now ALL_CAPS

--- Pokemon-data/src/main/java/com/karlson/pokemondata/util/GivePokemonType.java
+++ Pokemon-data/src/main/java/com/karlson/pokemondata/util/GivePokemonType.java
@@ -6,10 +6,13 @@ import java.util.Random;
 public class GivePokemonType {
 
     private static final List<Types> types = List.of(Types.values());
-    private static final int typeSize = types.size();
+    private static final int TYPE_SIZE = types.size();
 
     public static String get(Random random) {
-        return types.get(random.nextInt(typeSize)).toString();
+        return types.get(random.nextInt(TYPE_SIZE)).toString();
 
+    }
+
+    private GivePokemonType() {
     }
 }
