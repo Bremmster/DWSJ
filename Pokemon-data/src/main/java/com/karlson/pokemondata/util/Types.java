6c8f664e9c8596ff826364972f0a789644e7dd5d
Kristian Karlson <83krikar@gafe.molndal.se>
2023-09-22 15:50:56 +0200

    final now ALL_CAPS

--- Pokemon-data/src/main/java/com/karlson/pokemondata/util/Types.java
+++ Pokemon-data/src/main/java/com/karlson/pokemondata/util/Types.java
@@ -1,6 +1,6 @@
 package com.karlson.pokemondata.util;
 
 public enum Types {
-    normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy
+    NORMAL, FIRE, WATER, GRASS, ELECTRIC, ICE, FIGHTING, POISON, GROUND, FLYING, PSYCHIC, BUG, ROCK, GHOST, DRAGON, DARK, STEEL, FAIRY
 
 }
