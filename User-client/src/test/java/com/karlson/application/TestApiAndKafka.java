7d27c098b40a96ab2a19cc1bee403d94e473bc31
Kristian Karlson <83krikar@gafe.molndal.se>
2023-10-09 17:47:36 +0200

    mocked tests

--- User-client/src/test/java/com/karlson/application/TestApiAndKafka.java
+++ User-client/src/test/java/com/karlson/application/TestApiAndKafka.java
@@ -6,6 +6,7 @@ import com.karlson.service.HttpClient;
 import org.apache.hc.core5.http.HttpException;
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -17,21 +18,26 @@ import static org.mockito.Mockito.when;
 class TestApiAndKafka {
 
     private static Pokemon testPokemon;
-    private static HttpClient mockHttpClient; // = mock(HttpClient.class);
+    private static HttpClient mockHttpClient;
+    private static KafkaConsumer mockKafkaConsumer;
     private final HttpClient httpClient = new HttpClient();
     private final KafkaConsumer kafkaConsumer = new KafkaConsumer();
 
     @BeforeAll
     static void beforeAll() {
         testPokemon = new Pokemon("testPokemon", new Random());
-//        Create a pokemon to send  todo
-//    Create somthing that can compare to?
+
         mockHttpClient = mock(HttpClient.class);
-        try {
+        try { // todo get rid of try catch
             when(mockHttpClient.post(testPokemon)).thenReturn(200);
         } catch (HttpException e) {
             throw new RuntimeException(e);
         }
+
+        mockKafkaConsumer = mock(KafkaConsumer.class);
+        List<Pokemon> sutPokemonList = new ArrayList<>();
+        sutPokemonList.add(testPokemon);
+        when(mockKafkaConsumer.fetchKafkaData(false)).thenReturn(sutPokemonList);
     }
 
     @Test
@@ -48,8 +54,23 @@ class TestApiAndKafka {
         assertEquals(expected, ActualResponse);
     }
 
+
     @Test
     @Order(2)
+    void mockKafkaConsumerTest() {
+        // Arrange
+        String name = testPokemon.getName();
+        int total = testPokemon.getTotal();
+        // Act
+        List<Pokemon> pokemonList = mockKafkaConsumer.fetchKafkaData(false);
+        Pokemon kafkaPokemon = pokemonList.get(pokemonList.size() - 1);
+        // Assert
+        assertEquals(name, kafkaPokemon.getName());
+        assertEquals(total, kafkaPokemon.getTotal());
+    }
+
+    @Test
+    @Order(3)
     @Disabled
         // Warning! Test creates post in production. requires server
     void webApiTest() {
@@ -67,23 +88,7 @@ class TestApiAndKafka {
     @Test
     @Order(4)
     @Disabled
-        // Warning! Reads from kafka topic requires broker.
-    void mockKafkaConsumerTest() {
-        // Arrange
-        String name = testPokemon.getName();
-        int total = testPokemon.getTotal();
-        // Act todo mock this!
-        List<Pokemon> pokemonList = kafkaConsumer.fetchKafkaData(false);
-        Pokemon kafkaPokemon = pokemonList.get(pokemonList.size() - 1);
-        // Assert
-        assertEquals(name, kafkaPokemon.getName());
-        assertEquals(total, kafkaPokemon.getTotal());
-    }
-
-    @Test
-    @Order(4)
-    @Disabled
-        // Warning! Reads from kafka topic requires broker.
+        // Warning! Reads from kafka topic requires broker. Requires webApiTest() to succeed
     void kafkaConsumerTest() {
         // Arrange
         String name = testPokemon.getName();
