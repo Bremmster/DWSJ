a8bc2f234fae5856774895f3b7827525e66dfb3a
Kristian Karlson <83krikar@gafe.molndal.se>
2023-10-12 17:22:01 +0200

    handling Kakfa topic not available.

--- kafkaProducer-restApi/src/main/java/com/karlson/controller/PokemonMessageController.java
+++ kafkaProducer-restApi/src/main/java/com/karlson/controller/PokemonMessageController.java
@@ -2,13 +2,20 @@ package com.karlson.controller;
 
 import com.karlson.kafka.JsonKafkaProducer;
 import com.karlson.pokemondata.model.Pokemon;
+import org.apache.kafka.common.errors.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/pokemons")
 public class PokemonMessageController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PokemonMessageController.class);
     private final JsonKafkaProducer kafkaProducer;
 
     public PokemonMessageController(JsonKafkaProducer kafkaProducer) {
@@ -17,7 +24,13 @@ public class PokemonMessageController {
 
     @PostMapping("/publish")
     public ResponseEntity<String> publish(@RequestBody Pokemon pokemon) {
-        kafkaProducer.sendMessage(pokemon);
+
+        try {
+            kafkaProducer.sendMessage(pokemon);
+        } catch (TimeoutException e) {
+            LOGGER.warn("Kafka topic is down!");
+            return ResponseEntity.ofNullable("500");
+        }
         return ResponseEntity.ok("Message sent to topic");
     }
 }
